294128f21e5d0d58686db61408b3f6466e4e4cc8
Marie Otsuka <motsuka.gd@gmail.com>
2022-02-24 07:41:34 -0500

    styling and animation for cover page

--- scripts/script.js
+++ scripts/script.js
@@ -1,70 +1,115 @@
+/* setup dates library */
 let today = dayjs();
+const year = today.year();
+dayjs.extend(window.dayjs_plugin_isBetween);
 
-$(document).ready(function(){ 
+/* DOM elements */
+const poem = document.getElementById('poem');
+const spanfrom = document.getElementById('from');
+const spanto = document.getElementById('to');
+const spandays = document.getElementById('daysleft');
+const spanko = document.getElementById('ko');
+const spanweather = document.getElementById('weather');
 
-  const wghtmax = 700;
-  const wdthmax = 150;
+/* seasons data */
+let data;
+let season_index = 0;
 
-  //standard sliders
-  $('.axis-range').on('input', function(){
-
-    let value = parseInt($(this).val()); //get slider input value
-    // console.log(value);
-
-    const slidertype = $(this).data('type'); //get slider axis
-
-    console.log("--"+slidertype, value );
-
-    $('#poem').css("--"+slidertype, value);
-
-  });
-
-  $('#ss01').change(function(){
-    if( $(this).is(':checked') ) {
-      $('#poem').css('--ss01', 1);
-    } else {
-     $('#poem').css('--ss01', 0);
-    }
-  });
-
-  $('#ss03').change(function(){
-    if( $(this).is(':checked') ) {
-      $('#poem a').css({
-        '--ss03': 1,
-        'text-decoration': 'none'
-      });
-    } else {
-     $('#poem a').css({
-      '--ss03': 0,
-      'text-decoration': 'underline'
-    });
-    }
-  });
-
-  
-  getPoem();
-
-});
-
+/* typewriter options */
+let t = 0;
+const speed = 100; 
+let text = ""; 
 
 function getPoem( ){
   fetch("seasons.json")
     .then(response => response.json())
-    .then(json => displayPoem(json));
+    .then(json => {
+      data = json;
+      // console.log(data);
+
+      for(var i = 0; i<json.length; i++){
+          let season = json[i];
+          
+          //setup current year dates
+          let from = dayjs(year+'/'+season.start);  
+          let to = dayjs(year+'/'+season.end);
+          let toplus = to.add(1, 'days');
+
+          //determine current season by checking if today’s date falls
+          //between the start and end date
+          if( today.isBetween(from, toplus, null, '[)') ){
+            console.log(season);
+            season_index = i; // store index of current season
+            displayPoem(season);
+
+            let days = toplus.diff(today, 'day');
+            displayDaysLeft(days);
+            break;
+          }
+      }
+    });
 } 
 
-function displayPoem(data){
-  const poem = document.getElementById('poem');
-  dayjs.extend(window.dayjs_plugin_isBetween);
+function displayPoem(season){
+  resetTypeWriter();
+  text = season.en;
+  typeWriter();
 
-  for(var i = 0; i<data.length; i++){
-      let season = data[i]
-      let from = dayjs(season.start);  
-      let to = dayjs(season.end);
-      let toplus = to.add(1, 'days');
-      if( today.isBetween(from, toplus, null, '[)') ){
-        console.log(season);
-        poem.innerHTML = season.ko;
-      }
+  poem.style.setProperty('--wght', season.weight);
+  spanfrom.innerHTML = dayjs(season.start).format('MMMM D');
+  spanto.innerHTML = dayjs(season.end).format('MMMM D');
+  spanko.innerHTML = season.ko;
+  spanweather.innerHTML = season['sekki-en'];
+}
+
+function resetTypeWriter(){
+  poem.innerHTML = ""; //reset text
+  t = 0; //reset counter
+}
+
+function typeWriter( ) {
+  if (t < text.length) {
+    poem.innerHTML += text.charAt(t);
+    t++;
+    setTimeout(typeWriter, speed);
   }
 }
+
+function displayDaysLeft(days){
+  if (days == 1){
+    daysleft.innerHTML = "one day left";
+  }else if (days == 0){
+    daysleft.innerHTML = "today is the last day";
+  }else{
+    let numwords = ["two", "three", "four"];
+    let dayindex = days-2;
+    daysleft.innerHTML = numwords[dayindex] + " days left";
+  }
+}
+
+
+// display initial poem
+getPoem();
+
+// next and previous controls
+const next = document.getElementById('next');
+next.addEventListener('click', function(){
+  if (season_index < 71){
+    season_index++;    
+  }else{
+    season_index = 0;
+  }
+  displayPoem(data[season_index]);
+  daysleft.innerHTML = "";
+}, false);
+
+const back = document.getElementById('back');
+back.addEventListener('click', function(){
+  if (season_index > 0){
+    season_index--;    
+  }else{
+    season_index = 71;
+  }
+  displayPoem(data[season_index]);
+  daysleft.innerHTML = "";
+}, false);
